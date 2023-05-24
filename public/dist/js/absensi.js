72031556c495c0f255bb31150814c2a00dcff4e0
ferdihasan <ferdihasanpwd@gmail.com>
2023-05-24 22:48:21 +0700

    add faceid

--- public/dist/js/absensi.js
+++ public/dist/js/absensi.js
@@ -1,19 +1,48 @@
+import * as faceapi from 'face-api.js'
+// const faceapi = require('face-api.js')
+
 const video = document.getElementById('video')
 
-        // navigator usermedia has not supported
-        navigator.getUserMedia = ( navigator.getUserMedia ||
-            navigator.webkitGetUserMedia ||
-            navigator.mozGetUserMedia ||
-            navigator.msGetUserMedia);
+// navigator usermedia has not supported
+navigator.getUserMedia = ( navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia);
 
-        function startVideo() {
-            navigator.getUserMedia(
-                {video: {}},
-                stream => video.srcObject = stream,
-                err => console.error(err)
-            )
-        }
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models')
+]).then(startVideo);
 
-        startVideo()
-        console.log('ini skript')
-        // const radio = 'ini adalah script'
+
+
+function startVideo() {
+    navigator.getUserMedia(
+        {video: {}},
+        stream => video.srcObject = stream,
+        err => console.error(err)
+    )
+}
+
+video.addEventListener('play', () => {
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+    const displaySize = {width: video.width, height: video.height}
+    faceapi.matchDimensions(canvas, displaySize)
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks()
+        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+        faceapi.draw.drawDetections(canvas, resizedDetections)
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    }, 100)
+})
+
+console.log("test")
+
+
+
+
+
+        
--- resources/views/absensi.blade.php
+++ resources/views/absensi.blade.php
@@ -1,7 +1,13 @@
+<style>
+    canvas {
+        position: absolute;
+    }
+</style>
 @extends('layouts/main')
 @section('container')
     <h1>Absensi</h1>
     <video id="video" width="720" height="560" autoplay muted></video>
 
-    <script defer src="/dist/js/absensi.js"></script>
+    <script src="/dist/js/absensi.js" type="module"></script>
+    {{-- <script src="../js"></script> --}}
 @endsection
