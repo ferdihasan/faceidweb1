6ca792d7794da799ab549fbd18dd19152c47c97f
Ferdi Hasan <ferdihasanpwd@gmail.com>
2023-06-12 16:53:14 +0700

    fix table db faceid

--- database/migrations/2023_06_09_121600_create_faceids_table.php
+++ database/migrations/2023_06_09_121600_create_faceids_table.php
@@ -15,10 +15,10 @@ return new class extends Migration
     {
         Schema::create('faceids', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
             $table->string('karyawan_id');
             $table->longText('faceid1')->nullable();
             $table->longText('faceid2')->nullable();
+            $table->timestamps();
         });
     }
 
