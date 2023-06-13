9e9c4d54bb9e8c53ad68a541110709e492d958ae
Ferdi Hasan <ferdihasanpwd@gmail.com>
2023-06-13 18:12:44 +0700

    update create face id

--- resources/views/createfaceid.blade.php
+++ resources/views/createfaceid.blade.php
@@ -1,9 +1,17 @@
 {{-- {{ dd($karyawan->where('id', 1)) }} --}}
 {{-- <?php echo $karyawan ?> --}}
 @extends('layouts/main')
+<style>
+    canvas {
+        position: absolute;
+        top: 100px;
+        /* border: 1px solid black; */
+        right: 260px
+    }
+</style>
 @section('container')
     <div class="row g-3">
-        <div class="col-5 m-1" style="border: 1px solid black; border-radius: 5px; height: 500px">
+        <div class="col-5 m-1 shadow" style="border-radius: 5px; height: 500px">
             <form action="#" method="post">
                 @csrf
                 <div class="d-flex justify-content-center">
@@ -30,27 +38,23 @@
                     <label class="form-label" for="departemen">Departemen</label>
                     <input class="form-control" type="text" id="departemen" name="departemen" readonly>
                 </div>
-                <div class="mb-3">
-                    <label class="form-label" for="faceId">Face ID</label>
-
-                </div>
-                <div class="mb-3 d-flex justify-content-center">
-                    <button type="submit" class="btn btn-primary">Save</button>
-                </div>
             </form>
         </div>
-        <div class="col m-1" style="border: 1px solid black; border-radius: 5px">
+        <div class="col m-1 shadow" style="border-radius: 5px">
             <div class="d-flex justify-content-center">
-                <h1>test</h1>
+                <h3>Face ID</h3>
             </div>
-            <div class="d-flex justify-content-center">
-                <video id="video" height="320px" width="480px" style="border: 1px solid black" autoplay muted></video>
+            <div class="d-flex justify-content-center mb-3">
+                <video id="video" height="320px" width="480px" autoplay muted></video>
+            </div>
+            <div class="mb-3 d-flex justify-content-center">
+                <button type="submit" class="btn btn-primary">Save Face ID</button>
             </div>
         </div>
     </div>
 
-    <script defer src="dist/js/createfaceid.js"></script>
     <script defer src="dist/face-api.js/face-api.min.js"></script>
+    <script defer src="dist/js/createfaceid.js"></script>
 
     <script type="text/javascript">
         const onLoadEmail = () => {
